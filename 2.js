f528da0ddb3c2ffe05f5ebb8a6f6309ff70d0118
asmarkhatib <111004870+asmarkhatib@users.noreply.github.com>
2022-09-25 22:11:08 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -44,7 +44,7 @@ const isPrime = (number) => {
     if(number < 2) {
         return 'Not Prime';
     }
-    for(let i = 2; i <= number; i++){
+    for(let i = 2; i < number; i++){
         if(number % i === 0){
             return 'Not Prime';
         }
